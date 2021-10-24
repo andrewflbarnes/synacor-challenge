20e3b36b41f442026d35b1e3ebd820eeee0ad0aa
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-10-24 19:43:37 +0100

    Added a couple of short circuits to orb brute force

--- src/bin/orb/main.rs
+++ src/bin/orb/main.rs
@@ -2,6 +2,7 @@ use std::collections::VecDeque;
 
 struct Path {
     location: usize,
+    steps: u8,
     val: u64,
     route: String,
 }
@@ -83,6 +84,7 @@ fn main() {
 
     let start = Path {
         location: 6,
+        steps: 0,
         val: 22,
         route: "".into(),
     };
@@ -90,11 +92,24 @@ fn main() {
     let mut paths = VecDeque::new();
     paths.push_back(start);
 
+    let mut max_steps = 100;
+
     loop {
+        if paths.len() == 0 {
+            return;
+        }
+
         let path = paths.pop_front().expect("No paths to process!");
         let location = path.location;
-        
+
+        // no need to check any further steps on this path which would be >= max_steps + 1
+        // edge case where we have two matches on a row as we go from n steps to n+1 steps
+        if path.steps >= max_steps {
+            continue;
+        }
+
         for neighbour in &grid[location] {
+            // If underflow would occur ignore this path
             let negcheck = neighbour.2(100);
             if negcheck < 100 && (100 - negcheck) >= path.val {
                 continue;
@@ -102,17 +117,25 @@ fn main() {
 
             let next: Path = Path {
                 location: neighbour.0,
+                steps: path.steps + 1,
                 route: format!("{}{}", path.route, neighbour.1),
                 val: neighbour.2(path.val),
             };
 
-            if next.location == 1 {
-                println!("Reached vault with orb at value {}: {}", next.val, next.route);
-                if next.val == 30 {
-                    return;
-                }
+            // if we got to the vault with a value of 30, print the
+            if next.location == 1 && next.val == 30 {
+                max_steps = next.steps;
+                println!(
+                    "Reached vault with orb at value {} in {} steps:{}",
+                    next.val, max_steps, next.route
+                );
+                continue;
             }
 
+            // no need to check any further steps on this path which would be >= max_steps + 1
+            if path.steps >= max_steps {
+                continue;
+            }
 
             paths.push_back(next);
         }
